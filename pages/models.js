9c47d487bcfcc8b035d681b350b034c08afd1564
zepyroth3411 <brandones60@gmail.com>
2025-07-16 09:05:39 -0600

    add(models): Component of emotion_analizer

---
+++ pages/models.js
@@ -0,0 +1,9 @@
+import ThinkHubEmotionModelCard from "@/components/ThinkHubEmotionModelCard";
+
+export default function ModelsPage() {
+  return (
+    <div className="p-4">
+      <ThinkHubEmotionModelCard />
+    </div>
+  );
+}
